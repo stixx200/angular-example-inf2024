13968c66b42449628d6bf3efa8980153865557b3
stixx200 <stixx200@gmail.com>
2025-02-21 13:11:00 +0100

    add product.type.ts

---
+++ src/app/product.type.ts
@@ -0,0 +1,33 @@
+export type Product = {
+  id: string;
+  title: string;
+  description: string;
+  category: 'beauty';
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  tags: string[];
+  brand: string;
+  sku: string;
+  weight: number;
+  dimensions: { width: number; height: number; depth: number };
+  warrantyInformation: string;
+  shippingInformation: string;
+  availabilityStatus: string;
+  reviews: {
+    rating: number;
+    comment: string;
+    date: string;
+    reviewerName: string;
+    reviewerEmail: string;
+  }[];
+  returnPolicy: string;
+  minimumOrderQuantity: string;
+  meta: object;
+  updatedAt: string;
+  barcode: string;
+  qrCode: string;
+  images: string[];
+  thumbnail: string;
+};
