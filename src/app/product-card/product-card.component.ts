32bbd9952ed83fa0a44651455a56adf605101e83
stixx200 <stixx200@gmail.com>
2025-02-28 09:17:01 +0100

    add @angular/material

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { Product } from '../product.type';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-card',
-  imports: [],
+  imports: [MatCardModule, CommonModule],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss',
 })
